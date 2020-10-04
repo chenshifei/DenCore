0bee5157c869d1dbbd2c56c5ac71bdb8a582d327
shifei.chen <chenshifei.j@gmail.com>
2020-10-04 19:57:12 +0200

    Add CircuitBoard classes

---
+++ Sources/DenCore/Calculator/CircuitBoard.swift
@@ -0,0 +1,158 @@
+//
+//  CircuitBoardComponents.swift
+//  DenCore
+//
+//  Created by Shifei Chen on 2020-10-04.
+//
+
+import Foundation
+
+// MARK: - CircuitBoard provider & consumers
+
+/// Protocol for the owner of the `CircuitBoard`.
+public protocol CircuitBoardSocket {
+    
+    /// The `CircuitBoard` provided by its owner, may not be `nil`.
+    var circuitBoard: CircuitBoard! { get }
+}
+
+/// Protocol for the components.
+/// They don't own the `Circuitboard`, instead other `CircuitBoardSocket` objects should inject it into them.
+/// - Note: Instances implementing this protocol should keep a weak references to the `CircuitBoard` object.
+public protocol CircuitBoardPin {
+    
+    /// Injects a `CircuitBoard` object into it.
+    /// - Parameter circuitBoard: The `CircuitBoard` object to inject.
+    func installOnCircuitBoard(_ circuitBoard: CircuitBoard)
+}
+
+// MARK: - DisplayUnit
+
+/// A `DisplayUnit` is an object responsible for displying the calculation output.
+/// They could also have an optional `CustomizedKey` object to extend the calcualtor's functionality.
+public protocol DisplayUnit {
+    
+    /// Allowed `OperatorKeys`. For example some displayunit may disable the default `sin` operator.
+    var enabledOperatorKeys: [OperatorKey] { get }
+    
+    /// A customizable key to extend the calcualtor's functionality.
+    var customizedKey: CustomizedKey? { get }
+    
+    /// Handles when a number result is delivered.
+    /// - Parameter result: The delivered result, may contains an error.
+    func numericOutputDelivered(_ result: ProcessorResult)
+    
+    /// Handles when the `Equal` function key is pressed. Indicates an euqation has been evaluated.
+    /// - Parameter result: The  result from the equation, may contains an error.
+    func equationEvaluated(result: ProcessorResult)
+    
+    /// Handels when the customized key is pressed.
+    /// - Note: The display unit itself should be responsible for any errors that might happen.
+    func customizedKeyPressed()
+}
+
+// MARK: - KeyboardUnit
+
+/// A `KeyboardUnit` is an object that handels all the inputs of the calculator.
+public protocol KeyboardUnit {
+    
+    /// Install a customized key on the keyboard.
+    /// - Parameter customizedKey: The customized key to install
+    func installCustomizedKey(_ customizedKey: CustomizedKey)
+    
+    /// Enable / Disable the customized key. Once it is disabled, it will not longer listen to keyboard events.
+    /// - Parameter enable: `true` to enable it, otherwise `false` to disable it.
+    func customizedKey(enable: Bool)
+    
+    /// Install the default operator keys onto the keyboard.
+    /// - Parameter operatorKeys: The default operators to be installed.
+    func installOperatorKeys(_ operatorKeys: [OperatorKey])
+}
+
+// MARK: - CircuitBoard
+
+/// A `Circuitboard` object is responsible for handeling the communication between the input `KeyboardUnit` and the output `DisplayUnit`.
+public class CircuitBoard {
+    
+    // MARK: Properties
+    
+    /// The internal processor
+    internal var processor = Processor()
+    
+    /// The display unit.
+    /// After setting it, the default operator keys and the customized key will be installed on the keyboard unit.
+    /// - Note: The display unit is not required. If it is not set, circuit board will discard all the output results.
+    public var displayUnit: DisplayUnit? {
+        didSet {
+            if let displayUnit = displayUnit {
+                installDisplayUnit(displayUnit)
+            }
+        }
+    }
+    
+    /// The keyboard unit.
+    /// After setting it, the default operator keys and the customized key from the current display unit will be installed on the keyboard unit.
+    public var keyboardUnit: KeyboardUnit? {
+        didSet {
+            if let displayUnit = displayUnit {
+                installDisplayUnit(displayUnit)
+            }
+        }
+    }
+    
+    // MARK: Functions
+    
+    /// Installs the default operator keys and the customized key (if exists) from the display unit to the keyboard unit.
+    /// - Parameter newDisplayUnit: The display unit to be installed.
+    internal func installDisplayUnit(_ newDisplayUnit: DisplayUnit) {
+        // Clear the processor states first.
+        onFunctionKeyPressed(.clear)
+        if let keyboard = keyboardUnit {
+            keyboard.installOperatorKeys(newDisplayUnit.enabledOperatorKeys)
+            if let customizedKey = newDisplayUnit.customizedKey {
+                keyboard.installCustomizedKey(customizedKey)
+                keyboard.customizedKey(enable: customizedKey.enabled)
+            }
+        }
+    }
+    
+    /// Enable / Disable the customized key on the keyboard.
+    /// - Parameter enable: `Bool` value indicating enable or not
+    public func customizedKey(enable: Bool) {
+        if let keyboard = keyboardUnit {
+            keyboard.customizedKey(enable: enable)
+        }
+    }
+    
+    /// Handels when a numpad key is pressed on the keyboard.
+    /// - Parameter key: The  pressed`NumpadKey`.
+    public func onNumpadKeyPressed(_ key: NumpadKey) {
+        let result = processor.numpadKeyPressed(key: key)
+        displayUnit?.numericOutputDelivered(result)
+    }
+    
+    /// Handels when a opeartor key is pressed on the keyboard.
+    /// - Parameter key: The  pressed`OperatorKey`.
+    public func onOperatorKeyPressed(_ key: OperatorKey) {
+        let result = processor.operatorKeyPressed(key: key)
+        displayUnit?.numericOutputDelivered(result)
+    }
+    
+    /// Handels when the customized key is pressed on the keyboard.
+    public func onCustomizedKeyPressed() {
+        if displayUnit?.customizedKey?.enabled ?? false {
+            displayUnit?.customizedKeyPressed()
+        }
+    }
+    
+    /// Handels when a function key is pressed on the keyboard.
+    /// - Parameter key: The  pressed`FunctionKey`.
+    public func onFunctionKeyPressed(_ key: FunctionKey) {
+        let result = processor.functionKeyPressed(key: key)
+        if case .equal = key {
+            displayUnit?.equationEvaluated(result: result)
+        } else {
+            displayUnit?.numericOutputDelivered(result)
+        }
+    }
+}
---
+++ Tests/DenCoreTests/Calculator/CircuitBoardTest.swift
@@ -0,0 +1,169 @@
+//
+//  CircuitBoardTest.swift
+//  DenCoreTests
+//
+//  Created by Shifei Chen on 2020-10-04.
+//
+
+import XCTest
+@testable import DenCore
+
+final class MockDisplay: DisplayUnit {
+    
+    var display: String = ""
+    
+    var enabledOperatorKeys: [OperatorKey] = [DefaultKeys.Operator.add]
+    
+    var customizedKey: CustomizedKey?
+    
+    var customizedKeyPressCount = 0
+    
+    func numericOutputDelivered(_ result: ProcessorResult) {
+        onResultsDelivered(result)
+    }
+    
+    func equationEvaluated(result: ProcessorResult) {
+        onResultsDelivered(result)
+    }
+    
+    func customizedKeyPressed() {
+        customizedKeyPressCount += 1
+    }
+        
+    private func onResultsDelivered(_ result: ProcessorResult) {
+        if let error = result.1 {
+            display = error.localizedDescription
+        } else if let number = result.0 {
+            display = String(number)
+        }
+    }
+    
+}
+
+final class MockKeyboard: KeyboardUnit {
+    
+    var operatorKeys = [OperatorKey]()
+    var customizedKey: CustomizedKey?
+    
+    func installCustomizedKey(_ customizedKey: CustomizedKey) {
+        self.customizedKey = customizedKey
+    }
+    
+    func customizedKey(enable: Bool) {
+        self.customizedKey?.enabled = enable
+    }
+    
+    func installOperatorKeys(_ operatorKeys: [OperatorKey]) {
+        self.operatorKeys = operatorKeys
+    }
+}
+
+final class CircuitBoardTest: XCTestCase {
+    var circuitBoard = CircuitBoard()
+    
+    override func setUp() {
+        circuitBoard = CircuitBoard()
+    }
+    
+    func testInstallDisplayUnit() {
+        circuitBoard.onNumpadKeyPressed(.digit(underlyingValue: 1))
+        XCTAssertEqual(circuitBoard.processor.digitsRegister, [.digit(underlyingValue: 1)])
+        
+        let mockDisplay = MockDisplay()
+        let mockKeyboard = MockKeyboard()
+        circuitBoard.displayUnit = mockDisplay
+        circuitBoard.keyboardUnit = mockKeyboard
+        XCTAssertEqual(circuitBoard.processor.digitsRegister, [])
+        XCTAssertEqual(mockKeyboard.operatorKeys.first?.name, mockDisplay.enabledOperatorKeys.first?.name)
+        
+        let customizedKeyName = "stringify"
+        mockDisplay.customizedKey = CustomizedKey(name: customizedKeyName)
+        circuitBoard.displayUnit = mockDisplay
+        XCTAssertEqual(mockKeyboard.customizedKey?.name, customizedKeyName)
+    }
+    
+    func testOnNumpadKeyPressed() {
+        let mockDisplay = MockDisplay()
+        let mockKeyboard = MockKeyboard()
+        circuitBoard.displayUnit = mockDisplay
+        circuitBoard.keyboardUnit = mockKeyboard
+        
+        circuitBoard.onNumpadKeyPressed(.digit(underlyingValue: 1))
+        XCTAssertEqual(mockDisplay.display, "1.0")
+        circuitBoard.onNumpadKeyPressed(.digit(underlyingValue: 1))
+        XCTAssertEqual(mockDisplay.display, "11.0")
+        circuitBoard.onNumpadKeyPressed(.separator)
+        XCTAssertEqual(mockDisplay.display, "11.0")
+        circuitBoard.onNumpadKeyPressed(.digit(underlyingValue: 1))
+        XCTAssertEqual(mockDisplay.display, "11.1")
+    }
+    
+    func testOnOperatorKeyPressed() {
+        let mockDisplay = MockDisplay()
+        let mockKeyboard = MockKeyboard()
+        circuitBoard.displayUnit = mockDisplay
+        circuitBoard.keyboardUnit = mockKeyboard
+        
+        circuitBoard.onNumpadKeyPressed(.digit(underlyingValue: 1))
+        circuitBoard.onNumpadKeyPressed(.digit(underlyingValue: 1))
+        circuitBoard.onNumpadKeyPressed(.separator)
+        circuitBoard.onNumpadKeyPressed(.digit(underlyingValue: 1))
+        circuitBoard.onOperatorKeyPressed(DefaultKeys.Operator.add)
+        circuitBoard.onNumpadKeyPressed(.digit(underlyingValue: 1))
+        circuitBoard.onNumpadKeyPressed(.separator)
+        circuitBoard.onNumpadKeyPressed(.digit(underlyingValue: 1))
+        XCTAssertEqual(mockDisplay.display, "1.1")
+        circuitBoard.onOperatorKeyPressed(DefaultKeys.Operator.add)
+        XCTAssertEqual(mockDisplay.display, "12.2")
+    }
+    
+    func testOnFunctionKeyPressed() {
+        let mockDisplay = MockDisplay()
+        let mockKeyboard = MockKeyboard()
+        circuitBoard.displayUnit = mockDisplay
+        circuitBoard.keyboardUnit = mockKeyboard
+        
+        circuitBoard.onNumpadKeyPressed(.digit(underlyingValue: 1))
+        circuitBoard.onNumpadKeyPressed(.digit(underlyingValue: 1))
+        circuitBoard.onNumpadKeyPressed(.separator)
+        circuitBoard.onNumpadKeyPressed(.digit(underlyingValue: 1))
+        circuitBoard.onOperatorKeyPressed(DefaultKeys.Operator.add)
+        circuitBoard.onNumpadKeyPressed(.digit(underlyingValue: 1))
+        circuitBoard.onNumpadKeyPressed(.separator)
+        circuitBoard.onNumpadKeyPressed(.digit(underlyingValue: 1))
+        XCTAssertEqual(mockDisplay.display, "1.1")
+        circuitBoard.onFunctionKeyPressed(.equal)
+        XCTAssertEqual(mockDisplay.display, "12.2")
+        circuitBoard.onFunctionKeyPressed(.clear)
+        XCTAssertEqual(mockDisplay.display, "0.0")
+    }
+    
+    func testOnCustomizedKeyPressed() {
+        let mockDisplay = MockDisplay()
+        let mockKeyboard = MockKeyboard()
+        circuitBoard.displayUnit = mockDisplay
+        circuitBoard.keyboardUnit = mockKeyboard
+        
+        XCTAssertEqual(mockDisplay.customizedKeyPressCount, 0)
+        circuitBoard.onCustomizedKeyPressed()
+        XCTAssertEqual(mockDisplay.customizedKeyPressCount, 0)
+        mockDisplay.customizedKey = CustomizedKey(name: "stringify")
+        circuitBoard.displayUnit = mockDisplay
+        circuitBoard.onCustomizedKeyPressed()
+        XCTAssertEqual(mockDisplay.customizedKeyPressCount, 1)
+        mockDisplay.customizedKey?.enabled = false
+        circuitBoard.onCustomizedKeyPressed()
+        XCTAssertEqual(mockDisplay.customizedKeyPressCount, 1)
+    }
+    
+    func testEnableCustomizedKey() {
+        let mockDisplay = MockDisplay()
+        mockDisplay.customizedKey = CustomizedKey(name: "stringify")
+        let mockKeyboard = MockKeyboard()
+        circuitBoard.displayUnit = mockDisplay
+        circuitBoard.keyboardUnit = mockKeyboard
+        XCTAssert(mockKeyboard.customizedKey?.enabled ?? false)
+        circuitBoard.customizedKey(enable: false)
+        XCTAssertFalse(mockKeyboard.customizedKey?.enabled ?? true)
+    }
+}
